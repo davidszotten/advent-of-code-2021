e852a4fd7adafef999ee4e62b49f04c037f3a5b7
David Szotten <davidszotten@gmail.com>
2021-12-08 18:30:04 +0000

    tidy

--- src/bin/day08.rs
+++ src/bin/day08.rs
@@ -93,21 +93,6 @@ impl FromStr for Input {
     }
 }
 
-fn part1(input: &str) -> Result<usize> {
-    Ok(input
-        .lines()
-        .map(|l| l.parse::<Input>())
-        .collect::<Result<Vec<_>>>()?
-        .iter()
-        .map(|i| {
-            i.output
-                .iter()
-                .filter(|o| matches!(o.segments.len(), 2 | 3 | 4 | 7))
-                .count()
-        })
-        .sum())
-}
-
 /*
 
   0:      1:      2:      3:      4:
@@ -130,6 +115,70 @@ b    .  b    .  .    c  b    c  b    c
 
 */
 
+impl Input {
+    fn decode(self) -> Result<usize> {
+        let mut by_size: HashMap<usize, Vec<Digit>> = HashMap::new();
+        for pattern in self.patterns.into_iter() {
+            by_size
+                .entry(pattern.segments.len())
+                .or_insert(vec![])
+                .push(pattern);
+        }
+
+        let d1 = one(&by_size[&2])?;
+        let d4 = one(&by_size[&4])?;
+        let d7 = one(&by_size[&3])?;
+        let d8 = one(&by_size[&7])?;
+
+        let six_segments = &by_size[&6];
+        assert_eq!(six_segments.len(), 3);
+        let d6 = one(six_segments.iter().filter(|s| s.share(&d1, 1)))?;
+        let d9 = one(six_segments.iter().filter(|s| s.share(&d4, 4)))?;
+        let d0 = one(six_segments.iter().filter(|&s| s != &d6 && s != &d9))?;
+
+        let five_segments = &by_size[&5];
+        assert_eq!(five_segments.len(), 3);
+        let d3 = one(five_segments.iter().filter(|s| s.share(&d1, 2)))?;
+        let d5 = one(five_segments
+            .iter()
+            .filter(|&s| s != &d3 && s.share(&d6, 5)))?;
+        let d2 = one(five_segments
+            .iter()
+            .filter(|&s| s != &d3 && s.share(&d6, 4)))?;
+
+        let map = HashMap::from([
+            (d0, 0),
+            (d1, 1),
+            (d2, 2),
+            (d3, 3),
+            (d4, 4),
+            (d5, 5),
+            (d6, 6),
+            (d7, 7),
+            (d8, 8),
+            (d9, 9),
+        ]);
+        assert_eq!(map.len(), 10);
+
+        Ok(self.output.iter().fold(0, |acc, x| acc * 10 + map[x]))
+    }
+}
+
+fn part1(input: &str) -> Result<usize> {
+    Ok(input
+        .lines()
+        .map(|l| l.parse::<Input>())
+        .collect::<Result<Vec<_>>>()?
+        .iter()
+        .map(|i| {
+            i.output
+                .iter()
+                .filter(|o| matches!(o.segments.len(), 2 | 3 | 4 | 7))
+                .count()
+        })
+        .sum())
+}
+
 fn one<'a, I: IntoIterator<Item = &'a Digit>>(values: I) -> Result<Digit> {
     let mut it = values.into_iter();
     let item = it.next().context("was empty")?;
@@ -139,60 +188,13 @@ fn one<'a, I: IntoIterator<Item = &'a Digit>>(values: I) -> Result<Digit> {
     Ok(item.copy())
 }
 
-fn decode(input: &Input) -> Result<usize> {
-    let mut by_size: HashMap<usize, Vec<Digit>> = HashMap::new();
-    for pattern in input.patterns.iter().cloned() {
-        by_size
-            .entry(pattern.segments.len())
-            .or_insert(vec![])
-            .push(pattern);
-    }
-
-    let d1 = one(&by_size[&2])?;
-    let d4 = one(&by_size[&4])?;
-    let d7 = one(&by_size[&3])?;
-    let d8 = one(&by_size[&7])?;
-
-    let six_segments = &by_size[&6];
-    assert_eq!(six_segments.len(), 3);
-    let d6 = one(six_segments.iter().filter(|s| s.share(&d1, 1)))?;
-    let d9 = one(six_segments.iter().filter(|s| s.share(&d4, 4)))?;
-    let d0 = one(six_segments.iter().filter(|&s| s != &d6 && s != &d9))?;
-
-    let five_segments = &by_size[&5];
-    assert_eq!(five_segments.len(), 3);
-    let d3 = one(five_segments.iter().filter(|s| s.share(&d1, 2)))?;
-    let d5 = one(five_segments
-        .iter()
-        .filter(|&s| s != &d3 && s.share(&d6, 5)))?;
-    let d2 = one(five_segments
-        .iter()
-        .filter(|&s| s != &d3 && s.share(&d6, 4)))?;
-
-    let map = HashMap::from([
-        (d0, 0),
-        (d1, 1),
-        (d2, 2),
-        (d3, 3),
-        (d4, 4),
-        (d5, 5),
-        (d6, 6),
-        (d7, 7),
-        (d8, 8),
-        (d9, 9),
-    ]);
-    assert_eq!(map.len(), 10);
-
-    Ok(input.output.iter().fold(0, |acc, x| acc * 10 + map[x]))
-}
-
 fn part2(input: &str) -> Result<usize> {
     input
         .lines()
         .map(|l| l.parse::<Input>())
         .collect::<Result<Vec<_>>>()?
-        .iter()
-        .map(decode)
+        .into_iter()
+        .map(Input::decode)
         .sum()
 }
 
@@ -222,8 +224,8 @@ gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
     fn test_decode() -> Result<()> {
         let first =
             "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf";
-        let input = first.parse()?;
-        assert_eq!(decode(&input)?, 5353);
+        let input: Input = first.parse()?;
+        assert_eq!(input.decode()?, 5353);
         Ok(())
     }
 

55e1364d0834c440beac9a8e06ecff3d62b060cd
David Szotten <davidszotten@gmail.com>
2021-12-16 12:02:30 +0000

    day16 p1 wip

--- src/bin/day16.rs
+++ src/bin/day16.rs
@@ -7,18 +7,18 @@ fn main() -> Result<()> {
 
 #[derive(Debug)]
 enum Value {
-    Literal(i64),
+    Literal(usize),
     Op(Vec<Packet>),
 }
 
 #[derive(Debug)]
 struct Packet {
-    version: i64,
+    version: usize,
     value: Value,
 }
 
 impl Packet {
-    fn version_sum(&self) -> i64 {
+    fn version_sum(&self) -> usize {
         self.version
             + match &self.value {
                 Value::Literal(_) => 0,
@@ -40,8 +40,8 @@ fn hex_to_bytes(c: char) -> Result<Vec<u8>> {
     Ok(res)
 }
 
-fn bytes_to_dec(digits: &[u8]) -> i64 {
-    digits.iter().fold(0, |acc, d| acc * 2 + *d as i64)
+fn bytes_to_dec(digits: &[u8]) -> usize {
+    digits.iter().fold(0, |acc, d| acc * 2 + *d as usize)
 }
 
 fn take(it: &mut dyn Iterator<Item = u8>, n: usize) -> Result<Vec<u8>> {
@@ -53,7 +53,7 @@ fn take(it: &mut dyn Iterator<Item = u8>, n: usize) -> Result<Vec<u8>> {
     Ok(res)
 }
 
-fn parse(input: &str) -> Result<(Packet, i64)> {
+fn parse(input: &str) -> Result<(Packet, usize)> {
     let digits: Vec<u8> = input
         .trim()
         .chars()
@@ -62,22 +62,23 @@ fn parse(input: &str) -> Result<(Packet, i64)> {
         .into_iter()
         .flatten()
         .collect();
-    let mut it = digits.into_iter();
-    process(&mut it, 0)
+    process(&digits, 0)
 }
 
-fn process(it: &mut dyn Iterator<Item = u8>, depth: usize) -> Result<(Packet, i64)> {
+fn process(it: &[u8], depth: usize) -> Result<(Packet, usize)> {
+    // dbg!(it);
     let mut consumed = 0;
-    let version = bytes_to_dec(&take(it, 3).context("version")?);
+    let version = bytes_to_dec(&it[consumed..consumed + 3]);
     consumed += 3;
-    let type_id = bytes_to_dec(&take(it, 3).context("type_id")?);
+    let type_id = bytes_to_dec(&it[consumed..consumed + 3]);
     consumed += 3;
+    // dbg!(version, type_id);
     let value = match type_id {
         4 => {
             // literal
             let mut bin_parts = vec![];
             loop {
-                let chunk = take(it, 5).context("chunk")?;
+                let chunk = &it[consumed..consumed + 5];
                 consumed += 5;
                 bin_parts.push(chunk[1..].to_vec());
                 if chunk[0] == 0 {
@@ -88,10 +89,11 @@ fn process(it: &mut dyn Iterator<Item = u8>, depth: usize) -> Result<(Packet, i6
         }
         _ => {
             // operator
-            let length_type_id = it.next().context("no length_type_id")?;
+            let length_type_id = it[consumed];
+            // dbg!(length_type_id);
             consumed += 1;
             if length_type_id == 0 {
-                let sub_bits = bytes_to_dec(&take(it, 15).context("sub bits")?);
+                let sub_bits = bytes_to_dec(&it[consumed..consumed + 15]);
                 consumed += 15;
                 let mut seen_sub_bits = 0;
                 let mut packets = vec![];
@@ -100,9 +102,10 @@ fn process(it: &mut dyn Iterator<Item = u8>, depth: usize) -> Result<(Packet, i6
                 }
                 println!("looking for {} sub bits", sub_bits);
                 while seen_sub_bits < sub_bits {
-                    let (packet, bits) = process(it, depth + 1)?;
+                    let (packet, bits) = process(&it[consumed..], depth + 1)?;
                     packets.push(packet);
                     seen_sub_bits += bits;
+                    consumed += bits;
                 }
                 for _ in 0..depth {
                     print!(" ");
@@ -110,7 +113,7 @@ fn process(it: &mut dyn Iterator<Item = u8>, depth: usize) -> Result<(Packet, i6
                 println!("found {} sub bits", sub_bits);
                 Value::Op(packets)
             } else if length_type_id == 1 {
-                let sub_packets = bytes_to_dec(&take(it, 11).context("sub packets")?) as usize;
+                let sub_packets = bytes_to_dec(&it[consumed..consumed + 11]) as usize;
                 consumed += 11;
                 let mut packets = vec![];
                 for _ in 0..depth {
@@ -118,7 +121,8 @@ fn process(it: &mut dyn Iterator<Item = u8>, depth: usize) -> Result<(Packet, i6
                 }
                 println!("looking for {} sub packets", sub_packets);
                 while packets.len() < sub_packets {
-                    let (packet, _) = process(it, depth + 1)?;
+                    let (packet, bits) = process(&it[consumed..], depth + 1)?;
+                    consumed += bits;
                     packets.push(packet);
                 }
                 for _ in 0..depth {
@@ -135,7 +139,7 @@ fn process(it: &mut dyn Iterator<Item = u8>, depth: usize) -> Result<(Packet, i6
     // Ok(dbg!((Packet { version, value }, consumed)))
 }
 
-fn part1(input: &str) -> Result<i64> {
+fn part1(input: &str) -> Result<usize> {
     let (packet, _) = parse(input)?;
     // dbg!(&packet);
     Ok(packet.version_sum())
@@ -154,6 +158,7 @@ mod tests {
     #[test]
     fn test_hex_to_bytes() -> Result<()> {
         assert_eq!(hex_to_bytes('0')?, vec![0, 0, 0, 0]);
+        assert_eq!(hex_to_bytes('8')?, vec![1, 0, 0, 0]);
         assert_eq!(hex_to_bytes('9')?, vec![1, 0, 0, 1]);
         assert_eq!(hex_to_bytes('B')?, vec![1, 0, 1, 1]);
         Ok(())
@@ -163,8 +168,10 @@ mod tests {
     fn test_part1() -> Result<()> {
         // assert_eq!(part1(TEST_INPUT)?, 6);
         // assert_eq!(part1("38006F45291200")?, 9);
+        // assert_eq!(part1("EE00D40C823060")?, 14);
         // assert_eq!(part1("8A004A801A8002F478")?, 16);
         // assert_eq!(part1("620080001611562C8802118E34")?, 12);
+
         assert_eq!(part1("C0015000016115A2E0802F182340")?, 23);
         Ok(())
     }

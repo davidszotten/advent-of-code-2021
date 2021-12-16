e29f746f8fc9572a649ee76978280c7aac1cf4a0
David Szotten <davidszotten@gmail.com>
2021-12-16 15:50:11 +0000

    day16 p2

--- src/bin/day16.rs
+++ src/bin/day16.rs
@@ -6,9 +6,20 @@ fn main() -> Result<()> {
 }
 
 #[derive(Debug)]
+enum Op {
+    Sum,
+    Product,
+    Min,
+    Max,
+    GreaterThan,
+    LessThan,
+    EqualTo,
+}
+
+#[derive(Debug)]
 enum Value {
     Literal(usize),
-    Op(Vec<Packet>),
+    Op(Op, Vec<Packet>),
 }
 
 #[derive(Debug)]
@@ -22,8 +33,47 @@ impl Packet {
         self.version
             + match &self.value {
                 Value::Literal(_) => 0,
-                Value::Op(packets) => packets.iter().map(|p| p.version_sum()).sum(),
+                Value::Op(_, packets) => packets.iter().map(|p| p.version_sum()).sum(),
             }
+    }
+
+    fn value(&self) -> usize {
+        match &self.value {
+            Value::Literal(val) => *val,
+            Value::Op(Op::Sum, packets) => packets.iter().map(|p| p.value()).sum(),
+            Value::Op(Op::Product, packets) => packets.iter().map(|p| p.value()).product(),
+            Value::Op(Op::Min, packets) => packets
+                .iter()
+                .map(|p| p.value())
+                .min()
+                .expect("no packets for min"),
+            Value::Op(Op::Max, packets) => packets
+                .iter()
+                .map(|p| p.value())
+                .max()
+                .expect("no packets for min"),
+            Value::Op(Op::GreaterThan, packets) => {
+                if packets[0].value() > packets[1].value() {
+                    1
+                } else {
+                    0
+                }
+            }
+            Value::Op(Op::LessThan, packets) => {
+                if packets[0].value() < packets[1].value() {
+                    1
+                } else {
+                    0
+                }
+            }
+            Value::Op(Op::EqualTo, packets) => {
+                if packets[0].value() == packets[1].value() {
+                    1
+                } else {
+                    0
+                }
+            }
+        }
     }
 }
 
@@ -44,15 +94,6 @@ fn bytes_to_dec(digits: &[u8]) -> usize {
     digits.iter().fold(0, |acc, d| acc * 2 + *d as usize)
 }
 
-fn take(it: &mut dyn Iterator<Item = u8>, n: usize) -> Result<Vec<u8>> {
-    let mut res = vec![];
-    for _ in 0..n {
-        let n = it.next().context("not enough elements")?;
-        res.push(n);
-    }
-    Ok(res)
-}
-
 fn parse(input: &str) -> Result<(Packet, usize)> {
     let digits: Vec<u8> = input
         .trim()
@@ -66,13 +107,11 @@ fn parse(input: &str) -> Result<(Packet, usize)> {
 }
 
 fn process(it: &[u8], depth: usize) -> Result<(Packet, usize)> {
-    // dbg!(it);
     let mut consumed = 0;
     let version = bytes_to_dec(&it[consumed..consumed + 3]);
     consumed += 3;
     let type_id = bytes_to_dec(&it[consumed..consumed + 3]);
     consumed += 3;
-    // dbg!(version, type_id);
     let value = match type_id {
         4 => {
             // literal
@@ -87,66 +126,61 @@ fn process(it: &[u8], depth: usize) -> Result<(Packet, usize)> {
                 }
             }
         }
-        _ => {
+        op => {
             // operator
             let length_type_id = it[consumed];
-            // dbg!(length_type_id);
             consumed += 1;
-            if length_type_id == 0 {
+            let packets = if length_type_id == 0 {
                 let sub_bits = bytes_to_dec(&it[consumed..consumed + 15]);
                 consumed += 15;
                 let mut seen_sub_bits = 0;
                 let mut packets = vec![];
-                for _ in 0..depth {
-                    print!(" ");
-                }
-                println!("looking for {} sub bits", sub_bits);
                 while seen_sub_bits < sub_bits {
                     let (packet, bits) = process(&it[consumed..], depth + 1)?;
                     packets.push(packet);
                     seen_sub_bits += bits;
                     consumed += bits;
                 }
-                for _ in 0..depth {
-                    print!(" ");
-                }
-                println!("found {} sub bits", sub_bits);
-                Value::Op(packets)
+                packets
             } else if length_type_id == 1 {
                 let sub_packets = bytes_to_dec(&it[consumed..consumed + 11]) as usize;
                 consumed += 11;
                 let mut packets = vec![];
-                for _ in 0..depth {
-                    print!(" ");
-                }
-                println!("looking for {} sub packets", sub_packets);
                 while packets.len() < sub_packets {
                     let (packet, bits) = process(&it[consumed..], depth + 1)?;
                     consumed += bits;
                     packets.push(packet);
                 }
-                for _ in 0..depth {
-                    print!(" ");
-                }
-                println!("found {} sub packets", sub_packets);
-                Value::Op(packets)
+                packets
             } else {
                 bail!("invalid length_type_id");
-            }
+            };
+            Value::Op(
+                match op {
+                    0 => Op::Sum,
+                    1 => Op::Product,
+                    2 => Op::Min,
+                    3 => Op::Max,
+                    5 => Op::GreaterThan,
+                    6 => Op::LessThan,
+                    7 => Op::EqualTo,
+                    _ => bail!("unknown op type {}", op),
+                },
+                packets,
+            )
         }
     };
     Ok((Packet { version, value }, consumed))
-    // Ok(dbg!((Packet { version, value }, consumed)))
 }
 
 fn part1(input: &str) -> Result<usize> {
     let (packet, _) = parse(input)?;
-    // dbg!(&packet);
     Ok(packet.version_sum())
 }
 
-fn part2(_input: &str) -> Result<i64> {
-    Ok(0)
+fn part2(input: &str) -> Result<usize> {
+    let (packet, _) = parse(input)?;
+    Ok(packet.value())
 }
 
 #[cfg(test)]
@@ -166,13 +200,18 @@ mod tests {
 
     #[test]
     fn test_part1() -> Result<()> {
-        // assert_eq!(part1(TEST_INPUT)?, 6);
-        // assert_eq!(part1("38006F45291200")?, 9);
-        // assert_eq!(part1("EE00D40C823060")?, 14);
-        // assert_eq!(part1("8A004A801A8002F478")?, 16);
-        // assert_eq!(part1("620080001611562C8802118E34")?, 12);
-
+        assert_eq!(part1(TEST_INPUT)?, 6);
+        assert_eq!(part1("38006F45291200")?, 9);
+        assert_eq!(part1("EE00D40C823060")?, 14);
+        assert_eq!(part1("8A004A801A8002F478")?, 16);
+        assert_eq!(part1("620080001611562C8802118E34")?, 12);
         assert_eq!(part1("C0015000016115A2E0802F182340")?, 23);
+        Ok(())
+    }
+
+    #[test]
+    fn test_part2() -> Result<()> {
+        assert_eq!(part2("C200B40A82")?, 3);
         Ok(())
     }
 }
